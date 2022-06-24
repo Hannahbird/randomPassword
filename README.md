e22763f238e6edf1a5737dd2cf9fee95b29949ee
Hannah Brownson <hannahbrownson18@gmail.com>
2022-06-23 22:55:11 -0400

    Creating variables

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# randomPassword
+# Random Password Generator
+
+Create an application that generates a random password based on user-selected criteria. The app runs in the browser and features dynamically updated HTML and CSS powered by your JavaScript code. It has a clean and polished user interface and is responsive, which ensures that it adapts to multiple screen sizes.
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Global Variables
+var character = "!@#$%^&*()";
+var number = "0123456789";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// generate beginning password window, prompt selctions, user responses, random password generated as a result
+function generatePassword() {
+    var results = "";
+    var length = prompt("How many characters would you like the password to be?");
+    console.log(length);
+    if(length < 8 || length > 128) {
+        alert("Password must be more than 8 characters but no more than 128!");
+        return generatePassword();
+    } else{
+        // re-define variables with prompt windows
+        var characterCon = confirm("Would you like to include special characters?");
+        var numberCon = confirm("Would you like to include numbers?");
+        var lowerCon = confirm("Would you like to include lowercase letters?");
+        var upperCon = confirm("Would you like to include uppercase letters?");
+        
+    }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    
