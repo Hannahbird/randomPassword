f39fb5242b6f2eb30fed49d213c782a5b840df12
Hannah Brownson <hannahbrownson18@gmail.com>
2022-06-24 00:00:52 -0400

    Sike! now its final commit...i think

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Global Variables
-var character = "!@#$%^&*()";
+var character = "!@#$%^&*()_+-=";
 var number = "0123456789";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
